2f07f37cab4a64f207496cf363b6d8ef01231cce
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-09-19 18:03:33 +0300

    Init Files , Connect DB - Start The Sever

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDb } from './database/connectDb.js';
+import authRoutes from "./routes/auth.routes.js"
+
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use('/api/auth', authRoutes)
+
+app.listen(port, () => {
+    connectDb()
+    console.log('Server Running on ' + port)
+})
+
