572158e6a1658853bb6c51296583c891c6edbc04
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-09-20 18:15:17 +0300

    Sign Up

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,50 @@
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model.js";
+import { generateTokenSetCookie } from "../utils/generateTokenSetCookie.js";
+
 export const signup = async (req, res) => {
-    res.send('signup router')
+
+    const { email, password, name } = req.body;
+
+    try {
+
+        if (!email || !password || !name) {
+            throw new Error('Data is Missing')
+        }
+
+        const userExist = await User.findOne({ email })
+
+        if (userExist) {
+            return res.status(400).json({ success: false, message: "User Already Exist" })
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10)
+
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString()
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 7 * 24 * 60 * 60 * 1000 // 7days
+        })
+
+        await user.save();
+
+        // jwt 
+        generateTokenSetCookie(res, user._id)
+
+        res.status(201).json({
+            success: true, message: "User Created Successfully", user: {
+                ...user._doc,
+                password: undefined
+            }
+        })
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message })
+    }
 }
 
 export const login = async (req, res) => {
