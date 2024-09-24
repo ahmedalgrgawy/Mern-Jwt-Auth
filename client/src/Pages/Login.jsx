f93cda844f1efdae9a234d97f9c1da2cce5db99b
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-09-24 17:36:57 +0300

    Login Ui

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         <motion.div className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden' initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
             <div className="p-8">
                 <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
-                    Create Account
+                    Login
                 </h2>
 
                 <form onSubmit={handleLogin}>
