b18afa1c42efce41649984962a841de2e14c40bc
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-09-24 20:09:03 +0300

    Handling Cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { connectDb } from './database/connectDb.js';
 import authRoutes from "./routes/auth.routes.js"
 import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { connectDb } from './database/connectDb.js';
 
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
 app.use(express.json())
 
